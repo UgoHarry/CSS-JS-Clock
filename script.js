62222208010f37b5797e33efcbddb47c91305790
Ugo Anekwe <uanekwe@squiz.net>
2017-01-10 09:54:06 +1100

    Comments added to script

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 // FUNCTION THAT WILL RUN EVERY SECOND
 setInterval(doSecond, 1000);
-// setInterval(doMinute, 60000);
+// GET THE CURRENT MINUTE
 doMinute();
+// GET THE CURRENT HOUR
 doHour();
-function doSecond() {
-  //console.log("Hello");
-  const now = new Date();
 
-  // BREAK THE CLOCK CIRCLE DOWN INTO 60 PARTS, 1 PER SECOND
+function doSecond() {
+  const now = new Date();
   const seconds = now.getSeconds();
-  //console.log("Seconds: " + seconds);
+  // BREAK THE CLOCK CIRCLE DOWN INTO 60 PARTS, 1 PER SECOND
   const degPerSecond = ((seconds / 60)* 360) + 90;
-  //console.log("Degrees: "+degPerSecond);
   const secondHand = document.querySelector(".second-hand");
   // ROTATE SECOND HAND
   secondHand.style.transform = `rotate(${degPerSecond}deg)`;
-  if (degPerSecond == 90){
+  if (degPerSecond == 90){ // IF THE SECOND HAND GETS TO '12', RUN THE MINUTE FUNCTION
     doMinute();
   }
 }
@@ -23,11 +21,11 @@ function doSecond() {
 function doMinute() {
   const now = new Date();
   const minute = now.getMinutes();
-  console.log(minute);
+  // BREAK THE CLOCK CIRCLE DOWN INTO 60 PARTS, 1 PER MINUTE
   const degPerMinute = ((minute/60) * 360) + 90;
   const minuteHand = document.querySelector(".min-hand");
   minuteHand.style.transform = `rotate(${degPerMinute}deg)`;
-  if (degPerMinute == 90){
+  if (degPerMinute == 90){ // IF THE MINUTE HAND GETS TO '12', RUN THE HOUR FUNCTION
     doHour();
   }
 }
@@ -36,6 +34,7 @@ function doHour() {
   const now = new Date();
   const hour = now.getHours();
   console.log(hour);
+  // BREAK THE CLOCK CIRCLE DOWN INTO 12 PARTS, 1 PER HOUR
   const degPerHour = ((hour/12) * 360) + 90;
   const hourHand = document.querySelector(".hour-hand");
   hourHand.style.transform = `rotate(${degPerHour}deg)`;
